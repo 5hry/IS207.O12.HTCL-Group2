083f98dd9ae31de9741e6dbeae57c75ac0c28425
DESKTOP-VCDRFST\SANGNT <ngothanhsang000@gmail.com>
2024-05-29 00:50:43 +0700

    first commit

---
+++ public/js/firebase-config.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAECTRkmgAnY3ba8cBUO3L9qUC5VafY0T8",
+  authDomain: "loginis207.firebaseapp.com",
+  projectId: "loginis207",
+  storageBucket: "loginis207.appspot.com",
+  messagingSenderId: "750160800260",
+  appId: "1:750160800260:web:d4196de54f88bdb7122351",
+  measurementId: "G-HJP9QPGJZB"
+};
+
